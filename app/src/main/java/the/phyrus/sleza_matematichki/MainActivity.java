54629e7b4f639aa14cbcd2da9e7e080d6a2db597
ThePhyrus2 <iamthephyrus@gmail.com>
2021-10-05 15:23:00 +0300

    Initial commit

---
+++ app/src/main/java/the/phyrus/sleza_matematichki/MainActivity.java
@@ -0,0 +1,14 @@
+package the.phyrus.sleza_matematichki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
